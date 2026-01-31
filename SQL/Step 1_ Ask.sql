5fdd22b1ffc80c0560403cdbe2742f1eae32c381
ssagastume11 <sagassergio123@gmail.com>
2026-01-31 12:04:04 -0500

    BigQuery SQL queries

--- SQL/Step 1_ Ask.sql
+++ SQL/Step 1_ Ask.sql
@@ -1,1 +1,9 @@
+-- Business Task:
+-- Analyze gaming behavior data to determine whether daily and weekly gaming habits have a positive, negative, or neutral impact on academic and workplace performance. The objective is to identify patterns between gaming intensity and key performance indicators such as productivity, focus, stress, sleep, and overall academic or work scores, and translate those insights into data-driven conclusions.
 
+-- Key Questions:
+-- 1. How do academic or work performance scores vary across different levels of gaming intensity (low, moderate, high)?
+-- 2. Is there a relationship between daily and weekly gaming hours and productivity levels?
+-- 3. How do sleep hours, stress levels, and focus levels differ based on gaming behavior?
+-- 4. Does moderate gaming correlate with neutral or positive performance outcomes to low or high gaming intesity?
+-- 5. Which gaming patterns appear to support better academic or workplace performance, and which may hinder it?
